4a86879fa2a6b021fa5f12f1946d3ef56db190f7
xiaohao <2214925227@qq.com>
2020-12-02 12:20:33 +0800

    初始化项目

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store =new Vuex.Store({
+    //定义状态
+    state:{},
+    //定义方法
+    modules:{},
+    actions:{},
+    //类似计算属性
+    getters:{}
+
+
+
+})
